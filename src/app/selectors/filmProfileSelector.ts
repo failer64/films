d0359665222579ca0fed6daa4f60351b3a170974
nik <failer64@rambler.ru>
2023-02-14 21:28:54 +0300

    Updates

--- src/app/selectors/filmProfileSelector.ts
+++
@@ -1,6 +0,0 @@
-import {RootState} from "../store";
-
-export const selectFilm = (state: RootState) =>
-    state.filmsProfile.film
-export const selectPremieres = (state: RootState) =>
-    state.homePage.premieres
--- src/app/selectors/filmsPageSelector.ts
+++
@@ -1,4 +0,0 @@
-import {RootState} from "../store";
-
-export const selectFilms = (state: RootState) =>
-    state.filmsPage.films
