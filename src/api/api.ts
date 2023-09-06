c24213c1dd311b7b8b53852769aecd38eccec353
failer64 <failer64@rambler.ru>
2023-09-06 22:34:14 +0300

    test

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,94 +1,94 @@
 import axios from "axios";
-import {FilmType} from "../types/types";
+import { FilmType } from "../types/types";
 
 const instance = axios.create({
-    baseURL: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/',
-    headers: {
-        'X-API-KEY': 'a16e5ea3-7e40-47b8-a8dd-0a3e2a36e67d',
-    }
+	baseURL: 'https://kinopoiskapiunofficial.tech/api/v2.2/films/',
+	headers: {
+		'X-API-KEY': 'a16e5ea3-7e40-47b8-a8dd-0a3e2a36e67d',
+	}
 });
 
 export const dataAPI = {
-    getCurrentFilm(id: number = 1) {
-        return (instance.get<FilmType>(`${id}`).then(response => response.data));
-    },
-    getFilms(genres: number = 1, page: number = 1) {
-        return (instance.get<FilmsFullType>(`?page=${page}` + `&genres=${genres}`)).then(response => response.data);
-    },
-    getTopFilms(filmType: TopFilmsQueryType, page: number = 1) {
-        return (instance.get<TopFilmsFullType>(`top?page=${page}&type=${filmType}`).then(response => response.data));
-    },
-    getMovies(id: number) {
-        return (instance.get(`${id}/videos`).then(response => response.data));
-    },
-    getImages(id: number, page: number = 1) {
-        return (instance.get<ImagesType>(`${id}/images?page=${page}`).then(response => response.data));
-    },
-    getPremieres(year: number, month: string) {
-        return (instance.get<PremieresFullType>(`/premieres?year=${year}` + (`&month=${month}`)))
-            .then(response => response.data);
-    },
-    // getUsers(pageSize: number = 5, currentPage: number = 1) {
-    //     return (
-    //         instance.get<GetUsersType>(`users?count=${pageSize}&page=${currentPage}`)
-    //             .then(response => response.data)
-    //     )
-    // },
+	getCurrentFilm(id: number = 1) {
+		return (instance.get<FilmType>(`${id}`).then(response => response.data));
+	},
+	getFilms(genres: number = 1, page: number = 1) {
+		return (instance.get<FilmsFullType>(`?page=${page}` + `&genres=${genres}`)).then(response => response.data);
+	},
+	getTopFilms(filmType: TopFilmsQueryType, page: number = 1) {
+		return (instance.get<TopFilmsFullType>(`top?page=${page}&type=${filmType}`).then(response => response.data));
+	},
+	getMovies(id: number) {
+		return (instance.get(`${id}/videos`).then(response => response.data));
+	},
+	getImages(id: number, page: number = 1) {
+		return (instance.get<ImagesType>(`${id}/images?page=${page}`).then(response => response.data));
+	},
+	getPremieres(year: number, month: string) {
+		return (instance.get<PremieresFullType>(`/premieres?year=${year}` + (`&month=${month}`)))
+			.then(response => response.data);
+	},
+	// getUsers(pageSize: number = 5, currentPage: number = 1) {
+	//     return (
+	//         instance.get<GetUsersType>(`users?count=${pageSize}&page=${currentPage}`)
+	//             .then(response => response.data)
+	//     )
+	// },
 }
 export type PremieresFullType = {
-    total: number
-    items: Array<PremieresType>
+	total: number
+	items: Array<PremieresType>
 }
 export type PremieresType = {
-    kinopoiskId: number
-    nameRu: string
-    nameEn: string
-    year: number
-    posterUrl: string
-    posterUrlPreview: string
-    countries: Array<{
-        country: string
-    }>,
-    genres: Array<{
-        genre: string
-    }>,
-    duration: number
-    premiereRu: string
+	kinopoiskId: number
+	nameRu: string
+	nameEn: string
+	year: number
+	posterUrl: string
+	posterUrlPreview: string
+	countries: Array<{
+		country: string
+	}>,
+	genres: Array<{
+		genre: string
+	}>,
+	duration: number
+	premiereRu: string
 }
 
 export type TopFilmsFullType = {
-    pagesCount: number
-    films: Array<TopFilmsType>
+	pagesCount: number
+	films: Array<TopFilmsType>
 }
 export type TopFilmsType = {
-    countries: Array<{
-        country: string
-    }>,
-    filmId: number
-    filmLength: string
-    genres: Array<{
-        genre: string
-    }>,
-    nameEn: string
-    nameRu: string
-    posterUrl: string
-    posterUrlPreview: string
-    rating: string
-    ratingChange: null
-    ratingVoteCount: number
-    year: string
+	countries: Array<{
+		country: string
+	}>,
+	filmId: number
+	filmLength: string
+	genres: Array<{
+		genre: string
+	}>,
+	nameEn: string
+	nameRu: string
+	posterUrl: string
+	posterUrlPreview: string
+	rating: string
+	ratingChange: null
+	ratingVoteCount: number
+	year: string
 }
 
 export type TopFilmsQueryType = 'TOP_250_BEST_FILMS' | 'TOP_100_POPULAR_FILMS' | 'TOP_AWAIT_FILMS'
 
 export type ImagesType = {
-    items: Array<{ imageUrl: string, previewUrl: string }>
-    total: number
-    totalPages: number
+	items: Array<{ imageUrl: string, previewUrl: string }>
+	total: number
+	totalPages: number
 }
 
 export type FilmsFullType = {
-    items: FilmType[]
-    total: number
-    totalPages: number
+	items: FilmType[]
+	total: number
+	totalPages: number
 }
--- src/app/filmsPage.ts
+++ src/app/filmsPage.ts
@@ -1,60 +1,60 @@
-import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit'
-import {dataAPI, FilmsFullType} from "../api/api";
-import {FilmType} from "../types/types";
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { dataAPI, FilmsFullType } from "../api/api";
+import { FilmType } from "../types/types";
 
 
 export const getFilms = createAsyncThunk<FilmsFullType, argsType>(
-    'filmsPage/getFilms',
-    async (args) => {
-        return await dataAPI.getFilms(args.genre, args.page);
-    },
+	'filmsPage/getFilms',
+	async (args) => {
+		return await dataAPI.getFilms(args.genre, args.page);
+	},
 )
 
 const initialState = {
-    films: [] as Array<FilmType>,
-    isFetching: false,
-    genre: 1,
+	films: [] as Array<FilmType>,
+	isFetching: false,
+	genre: 1,
 }
 
 export const filmsSlice = createSlice({
-    name: 'filmsPage',
-    initialState,
-    reducers: {
-        changeGenre: (state, action: PayloadAction<number>) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
-            state.genre = action.payload;
-        },
-    },
-    extraReducers: (builder) => {
-        // Add reducers for additional action types here, and handle loading state as needed
-        builder.addCase(getFilms.fulfilled, (state, action) => {
-            // Add user to the state array
-            // if (state.mode) {
-            //     state.films = action.payload.items;
-            //     state.mode = false;
-            // } else {
-            //     state.films.push(...action.payload.items);
-            // }
-            state.films = action.payload.items;
-            state.isFetching = false;
-        }).addCase(getFilms.pending, (state) => {
-            // Add user to the state array
-            state.isFetching = true;
-        })
-    },
+	name: 'filmsPage',
+	initialState,
+	reducers: {
+		changeGenre: (state, action: PayloadAction<number>) => {
+			// Redux Toolkit allows us to write "mutating" logic in reducers. It
+			// doesn't actually mutate the state because it uses the Immer library,
+			// which detects changes to a "draft state" and produces a brand new
+			// immutable state based off those changes
+			state.genre = action.payload;
+		},
+	},
+	extraReducers: (builder) => {
+		// Add reducers for additional action types here, and handle loading state as needed
+		builder.addCase(getFilms.fulfilled, (state, action) => {
+			// Add user to the state array
+			// if (state.mode) {
+			//     state.films = action.payload.items;
+			//     state.mode = false;
+			// } else {
+			//     state.films.push(...action.payload.items);
+			// }
+			state.films = action.payload.items;
+			state.isFetching = false;
+		}).addCase(getFilms.pending, (state) => {
+			// Add user to the state array
+			state.isFetching = true;
+		})
+	},
 })
 
 // Action creators are generated for each case reducer function
-export const {changeGenre} = filmsSlice.actions
+export const { changeGenre } = filmsSlice.actions
 
 export default filmsSlice.reducer
 
 type StateType = typeof initialState
 
 type argsType = {
-    genre: number
-    page: number
+	genre: number
+	page: number
 }
